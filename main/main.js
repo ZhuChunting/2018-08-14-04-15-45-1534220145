fae2a9004e4373fbf8de7439ea7d5b2b66fc6faa
ZhuChunting <763433567@qq.com>
2018-09-26 11:47:13 +0800

    remainder

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 // Write your cade below:
 module.exports = function main() {
-  
-};
+  };
+
+package remainder;
+import java.util.Scanner;
+
+public class ComputeRemainder {
+  public static void main(String[] args) {
+	  Scanner input=new Scanner (System.in);
+	  double a=input.nextDouble();
+	  double b=input.nextDouble();
+	  double remainder= (double) a % b;
+	  System.out.println("The remainder is" + remainder);
+	  
+  }
+}
