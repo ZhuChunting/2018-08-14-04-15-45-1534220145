da843044f281d5b2edff3ad4334a7c0531f73767
ZhuChunting <763433567@qq.com>
2018-09-26 21:11:33 +0800

    array

--- main/main.js
+++ main/main.js
@@ -1,7 +1,4 @@
 // Write your cade below:
-module.exports = function main() {
-  };
-
 package remainder;
 import java.util.Scanner;
 
@@ -15,3 +12,50 @@ public class ComputeRemainder {
 	  
   }
 }
+
+
+
+作业2
+package array;
+
+import java.util.Scanner;
+public class ComputeArray {
+public static void main(String[] args){
+int x=0;
+double avg; //平均数
+int sum=0; //所有数字之和
+Scanner input = new Scanner(System.in);
+System.out.println("请输入你要处理的数字的个数：");
+int n = input.nextInt();
+int[] arr = new int[n]; //定义一个n位的数组
+System.out.println("请输入你要处理的数字：");
+for(x = 0; x< n;x++){
+System.out.print("第"+(x+1)+"个数字：");
+arr[x] = input.nextInt();
+}
+
+double maximum=arr[0];
+for (int i=1;i<arr.length;i++) {
+	if (arr[i]>maximum) maximum=arr [i];
+}
+System.out.println("最大值是"+ maximum);
+
+double minimum=arr[0];
+for (int i=1;i<arr.length;i++) {
+	if (arr[i]<minimum) minimum=arr [i];
+}
+System.out.println("最大值是"+ minimum);
+
+for(n = 0;n<arr.length;n++){
+sum += arr[n]; //累加
+}
+avg = sum/n;
+System.out.println("平均数是："+avg);
+System.out.println("元素的个数是"+arr.length);
+
+}
+}
+
+
+
+
